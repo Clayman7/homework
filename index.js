b0630839ae1403fba1f505df67f3d61ab90c62be
ChristianTaboada <taboada.christian@gmail.com>
2023-02-12 17:00:01 -0300

    resuelto

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
     if(numero % 2 == 0){
-
+        return numero.toString(2)
     }
     else{
-        
+        return numero.toString(2)
+
     }
     }
