38bec97f989a5d87264b8c3c91ffe6a3393ca6c0
Victor Colquichagua <69781012+victor-cp@users.noreply.github.com>
2023-02-12 16:27:33 -0500

    Update index.js
    salvada

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
-=======
 function laCajaDePandora(numero) {
   // proximamente escribiremos codigo aqui
   if (numero % 2 === 0) {
@@ -10,12 +6,6 @@ function laCajaDePandora(numero) {
     return numero.toString(16);
   }
 }
-
-
-    if(numero % 2 == 0){
-        return numero.toString(2)
-
-    }
 
 
 function lara () { 
@@ -27,18 +17,13 @@ function lara () {
     return info
 }
 =======
-    }
-    }
-
-    function Christian(){
-        return{
-            nombre: 'Christian',
-            edad: 30,
-            nacionalidad: 'Argentina'
-        }
-    }
-    //maldito pull
-    //maldito request
+function Christian(){
+     return{
+         nombre: 'Christian',
+         edad: 30,
+         nacionalidad: 'Argentina'
+     }
+}
 =======
 
 
