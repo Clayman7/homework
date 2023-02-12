ee6e9001ef56428220d71f36d188d62d59af0ae3
unknown <josephflo.dev@gmail.com>
2023-02-12 17:37:04 -0300

    bugs resueltos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function laCajaDePandora(numero) {
   // proximamente escribiremos codigo aqui
   if (numero % 2 === 0) return numero.toString(2);
-  if (numero % 2 === 1) return numero.toString(16);
+  else return numero.toString(16);
 }
 
 const josephDev = () => {
