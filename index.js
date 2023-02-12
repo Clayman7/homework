f6597c4825bfe8fa142347856643c85e5541d3da
victor-cp <vcolquichaguap@uni.pe>
2023-02-12 14:47:30 -0500

    commit

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function laCajaDePandora(numero) {
   // proximamente escribiremos codigo aqui
-  return numero;
+  // Si es un numero par convertirlo a binario y si es impar convertirlo a hexadecimal
+
+  if (numero % 2) return "0x" + numero.toString(16);
+  return numero.toString(2);
 }
+
+console.log(laCajaDePandora(12));
+console.log(laCajaDePandora(13));
+console.log(laCajaDePandora(25));
