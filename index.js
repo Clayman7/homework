0681fa2846e048d89ce2806bb48f12e4d8c8a0d9
fraynilson2003 <fraynilson2003@gmail.com>
2023-02-12 14:38:21 -0500

    binario y decimal

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
-    }
-
-const prueba = ()=>{
-    //pruebita
-    //
+function laCajaDePandora(numero) {
+  // proximamente escribiremos codigo aqui
+  if (numero % 2 === 0) {
+    return numero.toString(2);
+  } else {
+    return numero.toString(16);
+  }
 }
 
-const pruebaBranch = ()=>{
-    //prueba rama Fray
-}
+const pruebaBranch = () => {
+  //prueba rama Fray
+};
