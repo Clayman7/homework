6af2adcbcb652d775bdc837c27073c96c0519ce9
unknown <josephflo.dev@gmail.com>
2023-02-12 17:20:24 -0300

    funcion caja de pandora y objeto terminado

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
-    console.log(numero)
-    }
+function laCajaDePandora(numero) {
+  // proximamente escribiremos codigo aqui
+  if (numero % 2 === 0) return numero.toString(2);
+  if (numero % 2 === 1) return numero.toString(16);
+}
+
+const josephDev = () => {
+  return {
+    name: "Joseph",
+    age: 31,
+    nacionality: "Argentina",
+  };
+};
